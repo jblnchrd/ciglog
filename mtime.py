079d9bbf1b3774b8f857c2408be11295f53d3101
Jake Blanchard <jblnchrd910@live.com>
2017-01-08 09:28:05 -0600

    Update mtime.py

--- mtime.py
+++ mtime.py
@@ -1,6 +1,11 @@
 # time functions
 import filehandler
 
+days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+log_dates = []
+Days = {"Mon":0, "Tue":0, "Wed":0, "Thu":0, "Fri":0, "Sat":0, "Sun":0}
+
+
 def time_diff(a, b): #Terrible code to subtract times. But hey, it works...
     ans = [0,0,0]
     ans2 = [0,0,0]
@@ -80,3 +85,63 @@ def get_utime(entry):
         ut = entry[25:]
         return float(ut)
     return 0.
+
+    
+def per_day(logf):
+    days_log = filehandler.get_days(logf)
+    log_dates = days_log
+    count = 0
+    day_count = [0,0,0,0,0,0,0]
+    day = 0
+    for i in range(len(days_log)-1):
+        if (days_log[i] in days):
+            ind = days.index(days_log[i])
+            day_count[ind] += 1
+    return day_count
+
+    
+def per_date(logf):
+    date_log = filehandler.get_days(logf, True)
+    date_list = single_dates(logf)
+    index = 0
+    indices = []
+    for i in xrange(len(date_list)):
+        c = date_list[i]
+        indices.append(date_log.count(c))
+    return indices
+        
+    
+   
+def single_dates(logf):
+    logs = filehandler.get_days(logf, True)
+    loglist = logs
+    dates = []
+    inds = []
+    i = 0
+    dates.append(logs[0])
+    for l in range(0, len(logs)-1):
+            
+        if(logs[l] != logs[l+1]):
+            dates.append(logs[l+1])
+    
+    return dates
+   
+
+def print_per_date(logf):
+    counts = per_date(logf)
+    date_list = single_dates(logf)
+    ind = 0
+    for num in counts:
+        print("{} entries for {}".format(num, date_list[ind]))
+        ind += 1
+        
+
+        
+def print_per_day(logf):
+    count = per_day(logf)
+    ind = 0
+    for num in count:
+        print("{} entries on {}".format(num, days[ind]))
+        ind += 1
+    return 1
+    
